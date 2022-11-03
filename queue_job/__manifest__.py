10837619fd2974b90d4f7d4d73a9abae4da3a9c4
truthfool <ishanranasingh@gmail.com>
2022-11-03 20:02:38 +0530

    updated the module

--- queue_job/__manifest__.py
+++ queue_job/__manifest__.py
@@ -1,18 +1,18 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html)
 
-
 {
     "name": "Job Queue",
-    "version": "14.0.2.3.0",
+    "version": "14.0.3.0.6",
     "author": "Camptocamp,ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/queue",
     "license": "LGPL-3",
     "category": "Generic Modules",
-    "depends": ["mail"],
+    "depends": ["mail", "base_sparse_field"],
     "external_dependencies": {"python": ["requests"]},
     "data": [
         "security/security.xml",
         "security/ir.model.access.csv",
+        "views/queue_job_assets.xml",
         "views/queue_job_views.xml",
         "views/queue_job_channel_views.xml",
         "views/queue_job_function_views.xml",
